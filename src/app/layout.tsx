4fbe1c6484780484a61f0c4299c9433d89e346d8
ayush577 <agandhi62@gmail.com>
2024-10-29 09:04:45 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} antialiased min-h-screen bg-dark-300 light`}
+        className={`${inter.className} antialiased min-h-screen light`}
       >
         {children}
       </body>
